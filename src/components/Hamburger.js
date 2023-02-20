34f8101372da9f2f658ae8f7558a18f0f308f685
hasibulislam999 <hasib143sl@gmail.com>
2023-02-20 11:44:53 +0600

    hamburger icon init

---
+++ src/components/Hamburger.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Hamburger = ({ setOpen, open }) => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="w-8 h-8 p-1 border rounded"
+        onClick={() => setOpen(!open)}
+      >
+        <path
+          fillRule="evenodd"
+          d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default Hamburger;
